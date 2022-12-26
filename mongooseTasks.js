1796fc8431409986a6d0b2ae92b61bc685f67853
Gubsky_Maxim -_- <96713818+Storm2754@users.noreply.github.com>
2022-12-27 02:40:07 +0300

    СОЗДАНИЕ МОДЕЛИ ДАННЫХ ПРИЛОЖЕНИЯ

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,17 +1,17 @@
-//для изучения Momgoose  
 
-var mongoose= require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/test1')
+var Dog = require("./models/dog").Dog
 
-var schema= mongoose.Schema({ name: String})
 
-schema.methods.meow= function(){
-console.log(this.get("name") + "сонная собака")
-}
-
-var Dog= mongoose.model('Dog', schema)
-
-var puppy= new Dog({ name: 'Мягкая '})
-puppy.save(function(err) {
-    puppy.meow()
+var dog = new Dog({
+title:"Мопся",
+nick: "mopsic"//создание имени документа
 })
+
+
+console.log(dog)
+dog.save(function(err, dog, affected){
+    console.log(dog.title)
+    })
+    
