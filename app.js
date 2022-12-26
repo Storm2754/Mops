fa5acf87a241e3a901d5139d5e2bb55247d5fabf
Gubsky_Maxim -_- <96713818+Storm2754@users.noreply.github.com>
2022-12-27 02:58:50 +0300

    ПОДКЛЮЧЕНИЕ БАЗЫ ДАННЫХ

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//подключение базы данных
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/mops')
+//конец
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
