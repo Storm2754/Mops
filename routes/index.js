961f4e19a1dc62ca039f7dc4c50a152d4ffa7496
Gubsky_Maxim -_- <96713818+Storm2754@users.noreply.github.com>
2022-12-27 13:24:38 +0300

    ЗАПОЛНЕНИЕ МЕНЮ ИЗ БАЗЫ ДАННЫХ

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Dod = require("../models/dog").Dog
 
-/* код для страниц */
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+Dog.find({},{_id:0,title:1,nick:1},function(err,menu){
+res.render('index', {
+title: 'Express',
+menu: menu
+                            });
+    })
+
 });
+
+module.exports= router;
+
 
 
 /*
