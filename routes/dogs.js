75e35db2555ea8789b0552d8cff2ce716e492809
Gubsky_Maxim -_- <96713818+Storm2754@users.noreply.github.com>
2022-12-27 13:23:10 +0300

    ЗАПОЛНЕНИЕ МЕНЮ ИЗ БАЗЫ ДАННЫХ

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,24 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var Dog = require("../models/dog").Dog
+var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Текст для нового мархрутизатора');
+  res.send('Новый маршрутизатор');
 });
 
-/* Страница героев */
+/* Страница ведущих */
 router.get('/:nick', function(req, res, next) {
-    Dog.findOne({nick:req.params.nick}, function(err,dog){
-    if(err) returnnext(err)
-    if(!dog) returnnext(newError("Нет такой страницы"))
-    res.render('dog', {
-    title: dog.title,
-    picture: dog.avatar,
-    desc: dog.desc
-    })
-        })
-    })
-    
-    
+  async.parallel([
+          function(callback){
+                Dog.findOne({nick:req.params.nick}, callback)
+          },
+          function(callback){
+                Dog.find({},{_id:0,title:1,nick:1},callback)
+          }
+      ],
+      function(err,result){
+          if(err) return next(err)
+          var dog = result[0]
+          var dogs = result[1] || []
+          if(!dog) return next(new Error("Нет такой страницы"))
+          res.render('dog', {
+              title: dog.title,
+              picture: dog.avatar,
+              desc: dog.desc,
+              menu: dogs
+          });
+      })
+})
+
+
+
 
 module.exports = router;
