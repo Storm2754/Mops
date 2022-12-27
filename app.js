2ab7927d46db76473b079c2b380d2212db00865d
Gubsky_Maxim -_- <96713818+Storm2754@users.noreply.github.com>
2022-12-27 03:11:44 +0300

    ОБРАБОТКА ПАРАМЕТРА В АДРЕСЕ

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/mops')
 //конец
 
+var dogs = require('./routes/dogs');//объявление нового маршрутизатора
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));//объявление public папки
+app.use('/dogs', dogs);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
