2401b88ed94b544b03bc3f8c92bfbee14290b6ef
Gubsky_Maxim -_- <96713818+Storm2754@users.noreply.github.com>
2022-12-27 03:19:55 +0300

    ИЗВЛЕЧЕНИЕ ДАННЫХ ИЗ БАЗЫ

--- routes/dogs.js
+++ routes/dogs.js
@@ -6,10 +6,19 @@ router.get('/', function(req, res, next) {
   res.send('Текст для нового мархрутизатора');
 });
 
-/* Страницы о мопсе */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-    });
+/* Страница героев */
+router.get('/:nick', function(req, res, next) {
+    Dog.findOne({nick:req.params.nick}, function(err,dog){
+    if(err) returnnext(err)
+    if(!dog) returnnext(newError("Нет такой страницы"))
+    res.render('dog', {
+    title: dog.title,
+    picture: dog.avatar,
+    desc: dog.desc
+    })
+        })
+    })
+    
     
 
 module.exports = router;
