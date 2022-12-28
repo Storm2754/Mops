3b3cf06aac770003dc4c6f7bce7f8b59a8a7b000
Gubsky_Maxim -_- <96713818+Storm2754@users.noreply.github.com>
2022-12-28 18:14:56 +0300

    ЗАКРЫТИЕ СТРАНИЦ ДЛЯ НЕЗАЛОГИНЕННОГО ПОЛЬЗОВАТЕЛЯ

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,28 +1,28 @@
 var express = require('express');
 var router = express.Router();
+
 var Dog = require("../models/dog").Dog
-var async = require("async")
+var checkAuth = require("./../middleware/checkAuth.js")
+//var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор');
 });
 
-/* Страница ведущих */
-
-
-
-router.get('/:nick', function(req, res, next) {
+/* Страницы о мопсе */
+router.get('/:nick' ,checkAuth, function(req, res, next) {
   Dog.findOne({nick:req.params.nick}, function(err,dog){
-      if(err) return next(err)
-      if(!dog) return next(new Error("Нет такого ведущего для поиска"))
-      res.render('dog', {
-          title: dog.title,
-          picture: dog.avatar,
-          desc: dog.desc
-      })
-  })
+        if(err) return next(err)
+        if(!dog) return next(new Error("Нет такого ведущего для поиска"))
+        res.render('dog', {
+            title: dog.title,
+            picture: dog.avatar,
+            desc: dog.desc
+        })
+    })
 })
+
 
 
 
