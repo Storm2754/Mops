2125d51ae248be9c896d166caffdbc8dd977c614
Gubsky_Maxim -_- <96713818+Storm2754@users.noreply.github.com>
2023-01-15 18:34:54 +0300

    ВЫВОД  ДАННЫХ НА ЭКРАНАХ

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,28 +1,34 @@
+
 var express = require('express');
 var router = express.Router();
-
-var Dog = require("../models/dog").Dog
-var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
+//var checkAuth = require("./../middleware/checkAuth.js")
 //var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор');
+    res.send('Новый маршрутизатор');
 });
 
-/* Страницы о мопсе */
-router.get('/:nick' ,checkAuth, function(req, res, next) {
-  Dog.findOne({nick:req.params.nick}, function(err,dog){
+/* Страница ведущих */
+router.get('/:nick' , function(req, res, next) {
+    db.query(`SELECT * FROM dogs WHERE dogs.nick = '${req.params.nick}'`, (err, dogs) => {
+        if(err) {
+        console.log(err);
         if(err) return next(err)
-        if(!dog) return next(new Error("Нет такого ведущего для поиска"))
-        res.render('dog', {
-            title: dog.title,
-            picture: dog.avatar,
-            desc: dog.desc
+        }else {
+            if(err) return next(err)
+            if(dogs.lenght == 0) return next(new Error("Нет такого обьекта"))
+            var dog = dogs[0];
+            res.render('dog', {
+                title: dog.title,
+                picture: dog.avatar,
+                desc: dog.about
+                })
+            }
         })
     })
-})
-
+    
 
 
 
